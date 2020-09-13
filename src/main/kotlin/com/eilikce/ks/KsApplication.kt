a3d696cc56067e774f74c3a4c4a13f43df182c5b
Eilikce <wanghw_eilikce@163.com>
2020-09-13 23:34:10 +0800

    Add init project

---
+++ src/main/kotlin/com/eilikce/ks/KsApplication.kt
@@ -0,0 +1,11 @@
+package com.eilikce.ks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KsApplication>(*args)
+}
